7c193308b1268fa75569f74f3c1ff29a4810e79d
Ona <no-reply@ona.com>
2026-01-07 05:09:14 +0000

    Add MainActivity and update manifest with launcher activity
    Co-authored-by: Ona <no-reply@ona.com>

---
+++ app/src/main/java/ar/teamx/waveteamy/MainActivity.kt
@@ -0,0 +1,11 @@
+package ar.teamx.waveteamy
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Empty launcher activity
+    }
+}
